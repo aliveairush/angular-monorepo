2b0450eb0f55e0b57594d490b4a5a1b26367fe42
Ainur Aimurzin <aliveairush@gmail.com>
2025-05-26 20:32:45 +0300

    Added taiga ui kit

--- apps/taiga-ui-kit-usage/src/app/app.component.ts
+++ apps/taiga-ui-kit-usage/src/app/app.component.ts
@@ -1,12 +1,26 @@
+import { TuiRoot, TuiTextfieldComponent } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TuiInputNumberDirective } from '@taiga-ui/kit';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    TuiRoot,
+    TuiTextfieldComponent,
+    TuiInputNumberDirective,
+    ReactiveFormsModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'taiga-ui-kit-usage';
+
+  protected readonly control = new FormControl<number | null>(
+    null,
+    Validators.required,
+  );
 }
