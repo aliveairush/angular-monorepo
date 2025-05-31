2b471e634a557998c91056b2cd091a324e04331d
Aimurzin Ainur <60921090+aliveairush@users.noreply.github.com>
2025-05-31 15:52:21 +0300

    feat: add simple noopAnimations (#5)
    * feat: add simple animation conveyor
    
    * feat: add simple noopAnimations

--- apps/angular-monorepo/src/app/app.component.ts
+++ apps/angular-monorepo/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   imports: [ RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0, scale: 0.7 }),
+        animate('1400ms ease-in', style({ opacity: 1, scale: 1 })),
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'angular-monorepo';
--- apps/angular-monorepo/src/main.ts
+++ apps/angular-monorepo/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+setTimeout(() => {
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err),
+  );
+}, 5000) // Just emilation slow download for animation to run
