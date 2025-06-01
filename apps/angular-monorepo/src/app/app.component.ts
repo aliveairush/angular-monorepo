9c97fd02fb28a95033262bd485f0d9824521668b
Ainur Aimurzin <aliveairush@gmail.com>
2025-06-01 15:37:58 +0300

    feat: styling css

--- apps/angular-monorepo/src/app/app.component.ts
+++ apps/angular-monorepo/src/app/app.component.ts
@@ -13,8 +13,8 @@ import {
   animations: [
     trigger('fadeIn', [
       transition(':enter', [
-        style({ opacity: 0, scale: 0.7 }),
-        animate('1400ms ease-in', style({ opacity: 1, scale: 1 })),
+        style({ opacity: 0, scale: 0.9 }),
+        animate('500ms ease-in', style({ opacity: 1, scale: 1 })),
       ]),
     ]),
   ],
