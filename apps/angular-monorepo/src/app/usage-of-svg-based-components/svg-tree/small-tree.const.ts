7d536374e31cbb2941c45f9628e50e8d8f57e2bc
Ainur Aimurzin <aliveairush@gmail.com>
2025-06-01 11:37:59 +0300

    feat: Change to lib and refactor

---
+++ apps/angular-monorepo/src/app/usage-of-svg-based-components/svg-tree/small-tree.const.ts
@@ -0,0 +1,38 @@
+
+export const smallTree = {
+  id: 1,
+  label: 'Node 1',
+  children: [
+    {
+      id: 2,
+      label: 'Node 2',
+      children: [
+        {
+          id: 3,
+          label: 'Node 3',
+        },
+        {
+          id: 130,
+          label: 'Node 130',
+        },
+      ],
+    },
+    {
+      id: 257,
+      label: 'Node 257',
+      children: [
+        {
+          id: 258,
+          label: 'Node 258',
+          children: [
+            {
+              id: 259,
+              label: 'Node 259',
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- apps/angular-monorepo/src/app/usage-of-svg-based-components/usage-of-svg-based-components.component.ts
+++ apps/angular-monorepo/src/app/usage-of-svg-based-components/usage-of-svg-based-components.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SvgChartBarComponent } from './svg-chart-bar/svg-chart-bar.component';
-import { SvgTreeComponent } from './svg-tree/svg-tree.component';
+ import { AngularSvgTreeComponent } from '@angular-monorepo/angular-svg-tree';
+import { largeTree } from './svg-tree/large-tree.const';
+import { tree } from './svg-tree/medium-tree.const';
+import { smallTree } from './svg-tree/small-tree.const';
 
 @Component({
   selector: 'app-usage-of-svg-based-components',
-  imports: [SvgChartBarComponent, SvgTreeComponent],
+  imports: [SvgChartBarComponent, AngularSvgTreeComponent],
   templateUrl: './usage-of-svg-based-components.component.html',
   styleUrl: './usage-of-svg-based-components.component.css',
 })
@@ -16,4 +19,7 @@ export class UsageOfSvgBasedComponentsComponent {
     { count: 20, name: 'html' },
     { count: 1, name: 'Express' },
   ];
+  protected readonly largeTree = largeTree;
+  protected readonly tree = tree;
+  protected readonly smallTree = smallTree;
 }
