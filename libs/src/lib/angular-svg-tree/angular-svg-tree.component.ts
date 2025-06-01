c93e6b78c36b2b2318de797106bfdf6cb5f35187
Ainur Aimurzin <aliveairush@gmail.com>
2025-06-01 15:12:06 +0300

    chore: some comments

--- libs/src/lib/angular-svg-tree/angular-svg-tree.component.ts
+++ libs/src/lib/angular-svg-tree/angular-svg-tree.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectorRef,
   Component,
   computed,
+  DestroyRef,
   ElementRef,
   input,
   InputSignal,
@@ -13,6 +14,8 @@ import {
   viewChild,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { fromEvent } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export interface TreeNode {
   id: number;
@@ -28,6 +31,10 @@ export interface PositionedNode extends TreeNode {
   children?: PositionedNode[];
 }
 
+/**
+ * This is representation of SVG based tree in Angular
+ * Used Reingold–Tilford Tree Layout Algorithm to draw trees with balances appearance
+ */
 @Component({
   selector: 'lib-angular-svg-tree',
   imports: [CommonModule],
@@ -35,7 +42,11 @@ export interface PositionedNode extends TreeNode {
   styleUrl: './angular-svg-tree.component.css',
 })
 export class AngularSvgTreeComponent implements AfterViewInit {
-  constructor(private ngZone: NgZone, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private readonly ngZone: NgZone,
+    private readonly cdr: ChangeDetectorRef,
+    private readonly destroy$: DestroyRef
+  ) {}
 
   canvasWidth = input<number>(600);
 
@@ -43,103 +54,152 @@ export class AngularSvgTreeComponent implements AfterViewInit {
 
   treeData: InputSignal<TreeNode> = input.required();
 
-  levelSpacingY = input<number>(100); // по умолчанию 100px
+  /**
+   * Space between Parent and Child leaf - Y axis
+   */
+  levelSpacingY = input<number>(100);
 
+  /**
+   * Emit PositionedNode which contains x, y positions
+   */
   leafClick = output<PositionedNode>();
 
-  viewBoxWidth = 1000;
-  viewBoxHeight = 1000;
-
-  readonly $$isPanning = signal(false);
-  readonly $$startX = signal(0);
-  readonly $$startY = signal(0);
-
-  readonly $viewBox = computed(() => {
-    return `${this.$$viewBoxX()} ${this.$$viewBoxY()} ${this.viewBoxWidth} ${
-      this.viewBoxHeight
-    }`;
-  });
-
+  /**
+   * get svg from template so we can add event listeners to it , like mousemove
+   * Dont use (mousemove) in the template - so we dont trigger Change Detections
+   */
   svgContainer: Signal<ElementRef | undefined> =
     viewChild<ElementRef>('svgContainer');
 
+  /**
+   * $$isPanning , Used to identify when we need to change canvas view on mousemove
+   */
+  readonly $$isPanning = signal(false);
+  readonly $$mouseStartX = signal(0);
+  readonly $$mouseStartY = signal(0);
+
+  readonly $viewBox = computed(() => {
+    return `${this.$$viewBoxX()} ${this.$$viewBoxY()} ${this.$$viewBoxWidth()} ${this.$$viewBoxHeight()}`;
+  });
+
+  /**
+   * $$flattenedTree - is one dimensional array of initial tree.
+   */
   readonly $$flattenedTree = signal<ReadonlyArray<PositionedNode>>([]);
 
   readonly $$viewBoxX = signal(0);
   readonly $$viewBoxY = signal(0);
+  readonly $$viewBoxWidth = signal(1000);
+  readonly $$viewBoxHeight = signal(1000);
 
   readonly $visibleNodes: Signal<ReadonlyArray<PositionedNode>> = computed(() =>
     this.$$flattenedTree().filter(
       (node) =>
         node.x + 100 >= this.$$viewBoxX() &&
-        node.x <= this.$$viewBoxX() + this.viewBoxWidth &&
+        node.x <= this.$$viewBoxX() + this.$$viewBoxWidth() &&
         node.y + 40 >= this.$$viewBoxY() &&
-        node.y <= this.$$viewBoxY() + this.viewBoxHeight
+        node.y <= this.$$viewBoxY() + this.$$viewBoxHeight()
     )
   );
 
+  /**
+   * tree with coordiates
+   */
+  readonly $$positionedTree = signal<PositionedNode | null>(null);
+
+  /**
+   * Visible links between nodes. We need to show the line contained in viewBox.
+   * Recalculation when visible nodes are changed.
+   */
   readonly $visibleLinks = computed(() => {
-    const all: { from: PositionedNode; to: PositionedNode }[] = [];
+    const allVisibleNodesConnections: Array<{
+      from: PositionedNode;
+      to: PositionedNode;
+    }> = [];
 
-    const visibleIds = new Set(this.$visibleNodes().map((n) => n.id));
+    const visibleNodeIds = new Set(this.$visibleNodes().map((n) => n.id));
 
+    // Run for every node
     for (const node of this.$$flattenedTree()) {
+      // node has no children skip
       if (!node.children) continue;
 
+      // Run for every child node
       for (const child of node.children) {
-        // если хотя бы один из двух узлов видим — добавим линию
-        if (visibleIds.has(node.id) || visibleIds.has(child.id)) {
-          all.push({ from: node, to: child });
+        // if visible nodes has child or parent , we push new line;
+        if (visibleNodeIds.has(node.id) || visibleNodeIds.has(child.id)) {
+          allVisibleNodesConnections.push({ from: node, to: child });
         }
       }
     }
-
-    return all;
+    return allVisibleNodesConnections;
   });
 
-  positionedTree: PositionedNode | null = null;
-
   onZoom(event: WheelEvent) {
+    // Disable page scroll
     event.preventDefault();
 
     const svg = this.svgContainer(); // Signal<SVGElement>
     if (!svg) return;
 
+    // Get getBoundingClientRect() for getting coordinates
     const svgRect = svg?.nativeElement.getBoundingClientRect();
 
-    // Координаты мыши относительно SVG
-    const mouseX = event.clientX - svgRect.left;
-    const mouseY = event.clientY - svgRect.top;
+    // getting mouse x coordinates in SVG, it is screen pixels
+    const xMouseInPixelsInSvg = event.clientX - svgRect.left;
+    const yMouseInPixelsInSvg = event.clientY - svgRect.top;
+
+    // Svg width in pixels
+    const svgWidthInPixels = svgRect.width;
+    const svgHeightInPixels = svgRect.height;
+    /**
+     * example:
+     * xMouseInPixels = 400px ,
+     * svgWidth = 800
+     * 400 / 800 = 0.5 so mouse is in middle
+     */
+    const howFarMouseXFromSvgPercentage =
+      xMouseInPixelsInSvg / svgWidthInPixels;
+    const howFarMouseYFromSvgPercentage =
+      yMouseInPixelsInSvg / svgHeightInPixels;
+
+    const xMouseInSvgUnitsInsideViewBox =
+      this.$$viewBoxX() + howFarMouseXFromSvgPercentage * this.$$viewBoxWidth();
+    const yMouseInSvgUnitsInsideViewBox =
+      this.$$viewBoxY() +
+      howFarMouseYFromSvgPercentage * this.$$viewBoxHeight();
 
     const scaleAmount = event.deltaY > 0 ? 1.1 : 0.9;
 
-    // Позиция мыши в системе координат viewBox
-    const viewBoxMouseX =
-      this.$$viewBoxX() + (mouseX / svgRect.width) * this.viewBoxWidth;
-    const viewBoxMouseY =
-      this.$$viewBoxY() + (mouseY / svgRect.height) * this.viewBoxHeight;
+    // Update viewBox width and height
+    const newWidth = this.$$viewBoxWidth() * scaleAmount;
+    const newHeight = this.$$viewBoxHeight() * scaleAmount;
+    this.$$viewBoxWidth.set(newWidth);
+    this.$$viewBoxHeight.set(newHeight);
 
-    // Обновим размеры viewBox
-    const newWidth = this.viewBoxWidth * scaleAmount;
-    const newHeight = this.viewBoxHeight * scaleAmount;
+    // Adjust the viewBox position so that the zoom happens centered at the cursor location, not in the middle of the screen
+    const dx =
+      xMouseInSvgUnitsInsideViewBox - howFarMouseXFromSvgPercentage * newWidth;
+    const dy =
+      yMouseInSvgUnitsInsideViewBox - howFarMouseYFromSvgPercentage * newHeight;
 
-    // Смещение, чтобы масштаб был вокруг курсора
-    const dx = viewBoxMouseX - (mouseX / svgRect.width) * newWidth;
-    const dy = viewBoxMouseY - (mouseY / svgRect.height) * newHeight;
-
-    this.viewBoxWidth = newWidth;
-    this.viewBoxHeight = newHeight;
     this.$$viewBoxX.set(dx);
     this.$$viewBoxY.set(dy);
-    this.cdr.markForCheck();
+    this.cdr.markForCheck(); // Update
   }
 
+  /**
+   * Start painting when mouse is down and save positions
+   */
   onMouseDown(event: MouseEvent) {
     this.$$isPanning.set(true);
-    this.$$startX.set(event.clientX);
-    this.$$startY.set(event.clientY);
+    this.$$mouseStartX.set(event.clientX);
+    this.$$mouseStartY.set(event.clientY);
   }
 
+  /**
+   * Stop painting when mouse is up
+   */
   onMouseUp() {
     this.$$isPanning.set(false);
   }
@@ -151,20 +211,19 @@ export class AngularSvgTreeComponent implements AfterViewInit {
     xTracker = { current: 0 },
     spacing = 120
   ): PositionedNode {
-    console.log('Tree rerender');
-
-    const charWidth = 7.5;
-    const paddingX = 20;
+    const charWidth = 7.5; // Assume charachter is 7.5 pixels. Can be converted to props
+    const paddingX = 20; // Padding inside box. Can be converted to props
     const width = node.label.length * charWidth + paddingX;
 
     const positionedNode: PositionedNode = {
       id: node.id,
       label: node.label,
       x: 0,
-      y: depth * this.levelSpacingY(),
+      y: 20 + depth * this.levelSpacingY(), // 20 is just a padding for root leaf. Can be converted to props
       width,
     };
 
+    // if node has no children centers the box around the current position
     if (!node.children || node.children.length === 0) {
       positionedNode.x = xTracker.current + width / 2;
       xTracker.current += width + spacing;
@@ -192,56 +251,76 @@ export class AngularSvgTreeComponent implements AfterViewInit {
     return positionedNode;
   }
 
-  flatten(root: PositionedNode | undefined): PositionedNode[] {
-    console.log('flatten');
-
+  /**
+   * flatten a tree into a flat array
+   */
+  private flatten(root: PositionedNode | null): PositionedNode[] {
     const result: PositionedNode[] = [];
     if (!root) return result;
 
-    const visit = (node: PositionedNode) => {
-      result.push(node);
-      if (Array.isArray(node.children)) {
-        node.children.forEach(visit);
-      }
-    };
+    this.traverseTree(root, result);
 
-    visit(root);
     return result;
+  }
+
+  /**
+   * recursively visit each node
+   */
+  private traverseTree(node: PositionedNode, result: PositionedNode[]): void {
+    result.push(node);
+
+    for (const child of node.children ?? []) {
+      this.traverseTree(child, result);
+    }
   }
 
   onMouseMove(event: MouseEvent) {
     if (this.$$isPanning()) {
-      const dx = this.$$startX() - event.clientX;
-      const dy = this.$$startY() - event.clientY;
+      const dx = this.$$mouseStartX() - event.clientX;
+      const dy = this.$$mouseStartY() - event.clientY;
 
       this.$$viewBoxX.set(this.$$viewBoxX() + dx);
       this.$$viewBoxY.set(this.$$viewBoxY() + dy);
 
-      this.$$startX.set(event.clientX);
-      this.$$startY.set(event.clientY);
+      this.$$mouseStartX.set(event.clientX);
+      this.$$mouseStartY.set(event.clientY);
       this.cdr.markForCheck();
     }
   }
 
   ngAfterViewInit(): void {
-    this.positionedTree = this.layoutTree(this.treeData());
+    this.$$positionedTree.set(this.layoutTree(this.treeData()));
 
-    this.$$flattenedTree.set(this.flatten(this.positionedTree));
+    this.$$flattenedTree.set(this.flatten(this.$$positionedTree()));
 
+    /**
+     * Calculations does not need to trigger cdr
+     */
     this.ngZone.runOutsideAngular(() => {
-      console.log('svgContainer', this.svgContainer);
       const svg = this.svgContainer()?.nativeElement;
       if (!svg) return;
 
-      svg.addEventListener('mousedown', (e: MouseEvent) => this.onMouseDown(e));
-      svg.addEventListener('mousemove', (e: MouseEvent) => this.onMouseMove(e));
-      svg.addEventListener('mouseup', () => this.onMouseUp());
-      svg.addEventListener('wheel', (e: WheelEvent) => this.onZoom(e), {
-        passive: false,
-      });
+      fromEvent<MouseEvent>(svg, 'mousedown')
+        .pipe(takeUntilDestroyed(this.destroy$))
+        .subscribe((e) => this.onMouseDown(e));
+
+      fromEvent<MouseEvent>(svg, 'mousemove')
+        .pipe(takeUntilDestroyed(this.destroy$))
+        .subscribe((e) => this.onMouseMove(e));
+
+      fromEvent<MouseEvent>(svg, 'mouseup')
+        .pipe(takeUntilDestroyed(this.destroy$))
+        .subscribe(() => this.onMouseUp());
+
+      fromEvent<WheelEvent>(svg, 'wheel')
+        .pipe(takeUntilDestroyed(this.destroy$))
+        .subscribe((e) => this.onZoom(e));
     });
   }
 
+  /**
+   * Send positioned node to parent component
+   */
   handleLeafClick(node: PositionedNode) {
     this.leafClick.emit(node);
   }
